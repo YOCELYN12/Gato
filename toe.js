17133dc177784e1fcc3a0846789bde9c299e7ae3
YOCELYN12 <yocelynrivera9@gmail.com>
2024-05-22 15:40:07 -0600

    VS2

--- toe.js
+++ toe.js
@@ -9,31 +9,38 @@ let cas_6 = document.getElementById("cas_6")
 let cas_7 = document.getElementById("cas_7")
 let cas_8 = document.getElementById("cas_8")
 
-
 let arregloCeldas = [cas_0, cas_1, cas_2, cas_3, cas_4, cas_5, cas_6, cas_7, cas_8]
+
+
+let juga=document.getElementById("turnoJugador")
 
 function jugador1() {
     arregloCeldas.forEach(casilla => casilla.addEventListener("click", function () {
-        casilla.innerHTML = "✖️"
+        casilla.innerHTML = "✖️", "⭕"
+        juga.innerHTML="✖️"
         maquina()
-        if (ganador()) {c 
+        if (ganador()) {
+            alert("El ganador es: " + ganadorF);
         }
+        
     }))
-
 }
 jugador1()
 
 
 function maquina() {
-    let espaciosVacios = arregloCeldas.filter(celda => celda.innerHTML == "")
-    let numeroAleaotorio = Math.floor(Math.random() * espaciosVacios.length)
-    if (espaciosVacios.length > 0) {
-        espaciosVacios[numeroAleaotorio].innerHTML = "⭕"
-    }
-
-
+    setTimeout(() => {
+        juga.innerHTML="⭕"
+        let espaciosVacios = arregloCeldas.filter(celda => celda.innerHTML == "")
+        let numeroAleaotorio = Math.floor(Math.random() * espaciosVacios.length)
+        if (espaciosVacios.length > 0) {
+            espaciosVacios[numeroAleaotorio].innerHTML = "⭕", "✖️"
+        }
+    }, 1000);
 }
 
+
+let ganadorF;
 function ganador() {
     const combinacionesGanadoras = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -43,11 +50,31 @@ function ganador() {
     for (let combis of combinacionesGanadoras) {
         let [espacio1, espacio2, espacio3] = combis
         if (arregloCeldas[espacio1].innerHTML && arregloCeldas[espacio1].innerHTML === arregloCeldas[espacio2].innerHTML && arregloCeldas[espacio3].innerHTML === arregloCeldas[espacio1].innerHTML) {
-            return true
+            ganadorF = arregloCeldas[espacio1].innerHTML;
+            return true;
         }
     }
-    return false
+    return false;
 }
+
+
+
+const btn = document.getElementById("reglas")
+const p = document.getElementById("p")
+
+btn.addEventListener("click", () => {
+    p.classList.add("texto")
+})
+
+const btn2 = document.getElementById("boton")
+
+function borrarGato() {
+    btn2.addEventListener("click", () => {
+        arregloCeldas.forEach(celda=>celda.innerHTML="")
+    })
+}
+borrarGato()
+
 
 
 
